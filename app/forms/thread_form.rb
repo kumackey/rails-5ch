2d1b2803a1ab257120755bd9be1e9405ba1359b7
kumackey <kumak1t09e0@gmail.com>
2020-04-27 16:21:22 +0900

    add validation to thread_form

--- app/forms/thread_form.rb
+++ app/forms/thread_form.rb
@@ -7,4 +7,8 @@ class ThreadForm
   attribute :useremail, :string
   attribute :title, :string
   attribute :body, :string
+
+  validates :username, presence: true, length: { maximum: 20 }
+  validates :body, presence: true, length: { maximum: 500 }
+  validates :title, presence: true, length: { maximum: 100 }
 end
--- spec/models/forms/thread_form_spec.rb
+++ spec/models/forms/thread_form_spec.rb
@@ -6,4 +6,31 @@ RSpec.describe ThreadForm, type: :model do
   it '有効なファクトリを持つこと' do
     expect(thread_form).to be_valid
   end
+
+  it '本文が無いときに無効なこと' do
+    thread_form = build(:thread_form, body: '')
+    thread_form.valid?
+    expect(thread_form.errors.messages[:body]).to include('を入力してください')
+  end
+
+  it '本文が最大文字数より長いときに無効なこと' do
+    max_word_count = 500
+    thread_form = build(:thread_form, body: 'a' * (max_word_count + 1))
+    thread_form.valid?
+    expect(thread_form.errors.messages[:body]).to include("は#{max_word_count}文字以内で入力してください")
+  end
+
+  it 'ユーザー名が最大文字数より長いときに無効なこと' do
+    max_word_count = 20
+    thread_form = build(:thread_form, username: 'a' * (max_word_count + 1))
+    thread_form.valid?
+    expect(thread_form.errors.messages[:username]).to include("は#{max_word_count}文字以内で入力してください")
+  end
+
+  it 'スレッドタイトルが最大文字数より長いときに無効なこと' do
+    max_word_count = 100
+    thread_form = build(:thread_form, title: 'a' * (max_word_count + 1))
+    thread_form.valid?
+    expect(thread_form.errors.messages[:title]).to include("は#{max_word_count}文字以内で入力してください")
+  end
 end
