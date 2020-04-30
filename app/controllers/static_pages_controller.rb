34d35fe0098adf78d05c2f83e6690af81388e382
kumackey <kumak1t09e0@gmail.com>
2020-04-30 11:17:19 +0900

    add latest thread and reply to top page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,3 +1,6 @@
 class StaticPagesController < ApplicationController
-  def top; end
+  def top
+    @thre = Thre.order(updated_at: :desc).limit(1).take
+    @reply = Reply.order(updated_at: :desc).limit(1).take
+  end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Static_pages', type: :request do
   it 'トップページ画面の取得に成功すること' do
+    create(:reply)
     get '/'
     expect(response).to have_http_status(200)
   end
