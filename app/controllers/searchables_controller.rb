f4e7b677abcfe4972bd4676c7e07082f5474db78
kumackey <kumak1t09e0@gmail.com>
2020-04-30 10:41:37 +0900

    add pagination to search result

--- app/controllers/searchables_controller.rb
+++ app/controllers/searchables_controller.rb
@@ -1,6 +1,8 @@
 class SearchablesController < ApplicationController
   def search
-    @thres = @search_form.search_threads
-    @replies = @search_form.search_replies.includes(:thre)
+    thread_count = 5
+    reply_count = 10
+    @thres = @search_form.search_threads.page(params[:thres_page]).per(thread_count)
+    @replies = @search_form.search_replies.includes(:thre).page(params[:reply_page]).per(reply_count)
   end
 end
